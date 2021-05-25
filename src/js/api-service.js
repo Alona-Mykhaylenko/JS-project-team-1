a2523f2e270e51e3ac1a95b20daff68379b6b3ca
Alona-Mykhaylenko <79852771+Alona-Mykhaylenko@users.noreply.github.com>
2021-05-25 16:08:37 +0200

    changes

--- src/js/api-service.js
+++ src/js/api-service.js
@@ -1,4 +1,4 @@
-const MainURL = 'api.openweathermap.org/data/2.5/';
+const MainURL = 'http://api.openweathermap.org/data/2.5/';
 const apiKey = 'b97bd7991b77ef6d55cfd7dd50b01e99';
 
 let location = 'Kyiv';
@@ -16,7 +16,7 @@ const fetchWeatherDataOneDay = () =>
 // Получить все данные за 5 дней
 
 const fetchWeatherDataFiveDays = () =>
-  fetch(`${MainURL}/forecast?units=metric&q=${location}&appid=${apiKey}`).then(response => {
+  fetch(`${MainURL}forecast?units=metric&q=${location}&appid=${apiKey}`).then(response => {
     if (response.ok) {
       return response.json();
     }
