575dcdbcfdf6a65fa98dbd644597df75a1da8d11
Maksym-1989 <zaksumy1989@gmail.com>
2021-05-31 15:44:21 +0300

    all-done

--- src/js/geolocation.js
+++ src/js/geolocation.js
@@ -1,3 +1,10 @@
+import { setLocation } from './api-service';
+import { renderOneDayMarkup } from './render-one-day-forecast';
+import { dataFiveDays } from './render-five-day-forecast';
+// import { setLocationImg, setImgBg } from './geolocation';
+
+
+
 const inputRef = document.querySelector('.search-city__input');
 const bodyRef = document.querySelector('body');
 
@@ -27,4 +34,41 @@ function setImgBg() {
   });
 }
 setImgBg();
+
 export { setImgBg, setLocationImg };
+
+//=====================================================ОПРЕДЕЛЕНИЕ ГЕОДАННЫХ=============================
+navigator.geolocation.getCurrentPosition(success, error)
+function error() {
+  setLocation();
+  renderOneDayMarkup();
+  dataFiveDays();
+  setLocationImg('Kiev');
+  setImgBg();
+}
+
+function success(position) {
+  const apikey = '993fb22893a947dbb2d0ca6e36241a91';
+  
+  fetch(`https://api.opencagedata.com/geocode/v1/json?q=${position.coords.latitude}+${position.coords.longitude}&key=${apikey}`)
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    return Promise.reject(`Ошибка! Такого города нет в списке!`);
+  })
+  .then(data => {
+    const myCity = data.results[0].components.city;
+    console.log(myCity);
+    setLocation(myCity);
+    renderOneDayMarkup();
+    dataFiveDays();
+    setLocationImg(myCity);
+    setImgBg();
+  })
+}
+
+
+
+
+
